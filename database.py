ac016599b79c7f7eb2559c9694cc38e13fdea1be
BuildTools <unconfigured@null.spigotmc.org>
2021-02-02 00:20:56 +0100

    renamed parser module due to py conflict. Tested and fixed database and writing into it

--- database.py
+++ database.py
@@ -1,23 +1,30 @@
-import sqlite3, telebot
+import sqlite3, telebot, datetime
 
 conn = sqlite3.connect(r'.\starship-flight-updates\starship.db')
 c = conn.cursor()
 
 def setup_database():
     c.execute('DROP TABLE closure')
-    c.execute("CREATE TABLE 'closure' ('id' INTEGER PRIMARY KEY AUTOINCREMENT,'begin' TIMESTAMP NOT NULL,'end' TIMESTAMP NOT NULL,'valid' BOOLEAN NOT NULL DEFAULT TRUE);")
+    c.execute("CREATE TABLE 'closure' ('id' INTEGER PRIMARY KEY AUTOINCREMENT,'begin' TIMESTAMP NOT NULL,'end' TIMESTAMP NOT NULL,'valid' BOOL);")
     conn.commit()
 
 def append_cameroncounty(data: list):
+    data_as_list = []
     for d in data:
+        data_as_list.append(tuple(d.values()))
         if c.execute('SELECT valid FROM closure WHERE begin = ? AND end = ?',(d['begin'],d['end'])).fetchone():   #in database
             if c.execute('SELECT valid FROM closure WHERE begin = ? AND end = ?',(d['begin'],d['end'])).fetchone()[0] != d['valid']:   #smth changed
-                telebot.send_message(None,'Closure change!') #TODO
+                print('Closure change')
+                #telebot.send_message(None,'Closure change!') #TODO
                 c.execute('UPDATE closure SET valid = ? WHERE begin = ? AND end = ?',(d['valid'],d['begin'],d['end']))
         else:   #not in database
-            c.execute('INSERT INTO closure(begin, end, valid) VALUES(?,?,?)',(d.values()))
-            telebot.send_message(None,'New Closure!') #TODO
-        for valid in c.execute('SELECT begin, end, valid FROM closure WHERE valid = TRUE').fetchall():
-            if valid not in d:
-                c.execute('UPDATE closure SET valid = FALSE WHERE begin = ? AND end = ? AND valid = ?', list(valid))
+            c.execute('INSERT INTO closure(begin, end, valid) VALUES(?,?,?)',list(list(d.values())))
+            conn.commit()
+            print('New Closure: ',list(d.values()))
+            #telebot.send_message(None,'New Closure!') #TODO
+    print(data_as_list)
+    for valid in c.execute('SELECT begin, end, valid FROM closure WHERE valid = TRUE').fetchall():  #update old valid closures -> FALSE
+        if (datetime.datetime.strptime(valid[0],'%Y-%m-%d %H:%M:%S'),datetime.datetime.strptime(valid[1],'%Y-%m-%d %H:%M:%S'),valid[2]) not in data_as_list:
+            print('not in data')
+            c.execute('UPDATE closure SET valid = FALSE WHERE begin = ? AND end = ? AND valid = ?', list(valid))
     conn.commit()
