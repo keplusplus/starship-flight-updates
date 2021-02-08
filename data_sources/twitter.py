6cba2f6378597ca22351f243d6310161d98792f7
keplusplus <k.eilebrecht@gmx.de>
2021-02-08 23:07:15 +0100

    Get authorization from dotenv

--- data_sources/twitter.py
+++ data_sources/twitter.py
@@ -1,16 +1,32 @@
 import requests
 import json
+from data_sources import dotenv_parser
 
 class Twitter:
     tweet_endpoint = 'https://api.twitter.com/2/users/:id/tweets'
     lookup_endpoint = 'https://api.twitter.com/2/users/by'
-    headers = {
-        'Authorization': 'Bearer ' + 'AAAAAAAAAAAAAAAAAAAAAOQxMgEAAAAA%2F4OCxCwP6%2FaHi8vM%2BE7U74lvVk4%3Dh4InETTGnW856zQToV97mUMSvxtiPJgigtxyR9aTbR8ekoj77n'
-    }
+    env = dotenv_parser.get_env('.env')
+    try:
+        headers = {
+            'Authorization': 'Bearer ' + env['TWITTER_BEARER_TOKEN']
+        }
+    except KeyError as e:
+        print('Cannot create Twitter object!')
+        print(e)
+        print('Check you .env file!')
+        exit(1)
 
     def __init__(self):
         self.accounts = []
+        self.latest_tweets = []
         # print(json.dumps(self.accounts, indent=2, sort_keys=False))
+
+    def __get_account(username):
+        response = requests.get(Twitter.lookup_endpoint + '?usernames=' + str.lower(username), headers=Twitter.headers).json()
+        try:
+            return response['data'][0]
+        except KeyError:
+            return
 
     def add_twitter_account(self, username):
         account = self.__get_account(username)
@@ -22,15 +38,3 @@ class Twitter:
         account = self.__get_account(username)
         while account in self.accounts:
             self.accounts.remove(account)
-
-    def get_twitter_accounts(self):
-        return self.accounts
-
-    def __get_account(self, username):
-        response = requests.get(Twitter.lookup_endpoint + '?usernames=' + str.lower(username), headers=Twitter.headers).json()
-        try:
-            return response['data'][0]
-        except KeyError:
-            return
-
-Twitter()
