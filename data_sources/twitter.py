9681f002f6614539bbede65b363a593aab8d6775
keplusplus <k.eilebrecht@gmx.de>
2021-02-08 23:43:18 +0100

    Added private method to fetch tweets

--- data_sources/twitter.py
+++ data_sources/twitter.py
@@ -1,5 +1,7 @@
 import requests
-import json
+from datetime import timedelta, datetime
+
+from requests.models import Response
 from data_sources import dotenv_parser
 
 class Twitter:
@@ -16,17 +18,32 @@ class Twitter:
         print('Check you .env file!')
         exit(1)
 
-    def __init__(self):
+    def __init__(self, timespan):
         self.accounts = []
-        self.latest_tweets = []
-        # print(json.dumps(self.accounts, indent=2, sort_keys=False))
+        self.latest_tweets = {}
+        self.timespan = timespan
 
-    def __get_account(username):
+    def __get_account(self, username):
         response = requests.get(Twitter.lookup_endpoint + '?usernames=' + str.lower(username), headers=Twitter.headers).json()
         try:
             return response['data'][0]
         except KeyError:
             return
+    
+    def __get_tweets(self, user_id):
+        try:
+            latest = self.latest_tweets[user_id]
+            response = requests.get(Twitter.tweet_endpoint.replace(':id', user_id) + '?since_id=' + latest, headers=Twitter.headers).json()
+        except KeyError:
+            dt = datetime.utcnow() - timedelta(minutes=self.timespan)
+            dtstr = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+            response = requests.get(Twitter.tweet_endpoint.replace(':id', user_id) + '?start_time=' + dtstr, headers=Twitter.headers).json()
+
+        if response['meta']['result_count'] > 0:
+            self.latest_tweets[user_id] = response['meta']['newest_id']
+            return response['data']
+        else:
+            return []
 
     def add_twitter_account(self, username):
         account = self.__get_account(username)
