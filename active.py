6cdadb11abc8a31e5285fb0e23b6f4b48ee390f8
Fliens <fliens@web.de>
2021-02-19 14:42:07 +0100

    Added discord and general message handling

--- active.py
+++ active.py
@@ -1,4 +1,4 @@
-import database, time, telebot, datetime
+import database, time, telebot, datetime, message
 from data_sources.weather import Weather
 from data_sources import twitter, youtube
 from threading import Thread
@@ -12,11 +12,11 @@ def manage_closures(inlastmin = 20):
             if x not in currently_active['closure']:
                 currently_active['closure'].append(x)
                 if x[0]+datetime.timedelta(minutes=inlastmin) > datetime.datetime.now()-datetime.timedelta(hours=1):
-                    telebot.send_channel_message('<b>Road closure now active!</b>\n(<i>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+' UTC</i>)'+Status().active_change(currently_active))
+                    message.send_message('<b>Road closure now active!</b>\n(<i>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+' UTC</i>)'+Status().active_change(currently_active))
     for x in currently_active['closure']:
         if x not in database.road_closure_active():
             currently_active['closure'].remove(x)
-            telebot.send_channel_message('<b>Road closure no longer active!</b>\n(<i><s>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+' </s>UTC</i>)'+Status().active_change(currently_active))
+            message.send_message('<b>Road closure no longer active!</b>\n(<i><s>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+' </s>UTC</i>)'+Status().active_change(currently_active))
 
 def manage_tfrs(inlastmin = 20):
     if database.faa_active() != []:
@@ -24,11 +24,11 @@ def manage_tfrs(inlastmin = 20):
             if x not in currently_active['tfr']:
                 currently_active['tfr'].append(x)
                 if x[0]+datetime.timedelta(minutes=inlastmin) > datetime.datetime.now()-datetime.timedelta(hours=1):
-                    telebot.send_channel_message('<b>TFR (unlimited) now active!</b>\n(<i>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+' UTC</i>)'+Status().active_change(currently_active))
+                    message.send_message('<b>TFR (unlimited) now active!</b>\n(<i>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+' UTC</i>)'+Status().active_change(currently_active))
     for x in currently_active['tfr']:
         if x not in database.faa_active():
             currently_active['tfr'].remove(x)
-            telebot.send_channel_message('<b>TFR (unlimited) no longer active!</b>\n(<i><s>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+'</s> UTC</i>)'+Status().active_change(currently_active))
+            message.send_message('<b>TFR (unlimited) no longer active!</b>\n(<i><s>From '+database.datetime_to_string(x[0])+' to '+database.datetime_to_string(x[1])+'</s> UTC</i>)'+Status().active_change(currently_active))
 
 def twitter_filter(user:str, text:str) -> bool:
     f = {
@@ -60,13 +60,13 @@ def manage_twitter(twit:twitter.Twitter):
             link = 'https://twitter.com/'+x+'/status/'+str(tweet['id'])
             #if x in ['elonmusk','SpaceX']:    #usernames where filter is needed
             if twitter_filter(x, tweet['text']):
-                telebot.send_channel_message('<a href="'+link+'">‌‌<u><b>Tweet by '+twit.get_Name(x)+' (@'+x+' on Twitter)</b></u></a>')
+                message.send_message('<a href="'+link+'">‌‌<u><b>Tweet by '+twit.get_Name(x)+' (@'+x+' on Twitter)</b></u></a>')
 
 def manage_youtube(yt:youtube.Youtube()):
     update = yt.update()
     if update is not None:
         for x in update:
-            telebot.send_channel_message('<a href="'+x+'">‌‌<u><b>New Video by SpaceX</b></u></a>')
+            message.send_message('<a href="'+x+'">‌‌<u><b>New Video by SpaceX</b></u></a>')
 
 def handle_elon(twit:twitter.Twitter):
     if twit.get_Name('elonmusk') is None and currently_active['closure']!=[] and currently_active['tfr']!=[]:
--- data_sources/weather.py
+++ data_sources/weather.py
@@ -2,7 +2,7 @@
 #https://openweathermap.org/weather-conditions#Weather-Condition-Codes-2
 #25.997083229714256, -97.15597286864448
 #http://history.openweathermap.org/data/2.5/history/city?lat={lat}&lon={lon}&type=hour&start={start}&end={end}&appid={API key}
-import requests, json, datetime, telebot
+import requests, json, datetime, telebot, message
 import status
 from data_sources import dotenv_parser
 
@@ -62,20 +62,20 @@ class Weather:
             if w is None: w = self.current_weather()
             if self.weather_text(w)[1] != self.weather_text(last)[1]:
                 if self.weather_text(w)[1]:
-                    telebot.send_channel_message('<b>Weather has changed:</b>\n<i>'+self.weather_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
+                    message.send_message('<b>Weather has changed:</b>\n<i>'+self.weather_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
                 else:
-                    telebot.send_channel_message('<b>Weather has changed:</b>\n<i>'+self.weather_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
+                    message.send_message('<b>Weather has changed:</b>\n<i>'+self.weather_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
             elif self.wind_text(w)[1] != self.wind_text(last)[1]:
                 if self.wind_text(w)[1]:
-                    telebot.send_channel_message('<b>Wind has changed:</b>\n<i>'+self.wind_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
+                    message.send_message('<b>Wind has changed:</b>\n<i>'+self.wind_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
                 else:
-                    telebot.send_channel_message('<b>Wind has changed:</b>\n<i>'+self.wind_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
+                    message.send_message('<b>Wind has changed:</b>\n<i>'+self.wind_text(w)[0]+'</i>'+status.Status().active_change(currently_active))
             elif self.weather_text(w)[1] != self.weather_text(last)[1] and self.wind_text(w)[1] != self.wind_text(last)[1]:
                 out = '<b>Weather and wind have changed:</b><i>\nWeather: '
                 out+= ('✅' if self.weather_text(w)[1]  else '❌')
                 out+= ' '+self.weather_text(w)[0]+'\nWind: '
                 out+= ('✅' if self.wind_text(w)[1]  else '❌')
                 out+= ' '+self.wind_text(w)[0]+'</i>'
-                telebot.send_channel_message(out+status.Status().active_change(currently_active))
+                message.send_message(out+status.Status().active_change(currently_active))
         except Exception as e:
             telebot.send_err_message('Error Weather-weather-change!\n\nException:\n' + str(e))
