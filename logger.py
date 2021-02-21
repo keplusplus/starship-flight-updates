e2cfdc746409720a907d9ee3466dec4d849c8dcb
keplusplus <k.eilebrecht@gmx.de>
2021-02-21 20:13:00 +0100

    Added logger module with StarshipLogger class

---
+++ logger.py
@@ -0,0 +1,40 @@
+import sys
+import logging
+
+class StarshipLogger:
+    def __init__(self, log_file_name = 'latest.log', level = logging.INFO):
+        self.log_file_name = log_file_name
+        self.level = level
+
+        self.logger = logging.getLogger()
+
+        file = open(self.log_file_name, 'w')
+        file.truncate(0)
+        file.close()
+
+        logFormatter = logging.Formatter("%(asctime)s [%(threadName)s] [%(levelname)s] | %(message)s")
+
+        fileHandler = logging.FileHandler('latest.log')
+        fileHandler.setFormatter(logFormatter)
+        self.logger.addHandler(fileHandler)
+
+        consoleHandler = logging.StreamHandler(sys.stdout)
+        consoleHandler.setFormatter(logFormatter)
+        self.rootLogger.addHandler(consoleHandler)
+
+        self.rootLogger.setLevel(logging.INFO)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def log(self, msg):
+        self.info(msg)
